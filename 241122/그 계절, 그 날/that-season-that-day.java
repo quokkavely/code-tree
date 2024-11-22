7eb1f4b272b37f2ebb96bf7c930077c1b7d0284a
Hyeji <165968530+quokkavely@users.noreply.github.com>
2024-11-22 11:33:08 +0900

    Title: 그 계절, 그 날, Time: 145ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241122/그 계절, 그 날/that-season-that-day.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        int m = sc.nextInt();
+        int d = sc.nextInt();
+
+        if(isValidDate(y, m, d)) {
+            System.out.println(seasonSelect(m));
+        } else {
+            System.out.println(-1);
+        }
+
+    }
+
+    private static boolean isValidDate (int y, int m, int d) {
+        
+        //2월 29일 제거
+        if (y % 4 != 0) {
+            if(m == 2 && d >= 29) {return false; }
+        }
+
+        if((m == 4 || m == 6 || m == 9 || m == 11) && d > 30) {
+            return false;
+        } else if (d > 31) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static String seasonSelect (int m) {
+        String answer = "";
+
+        if (3 <= m && m <= 5) {
+            answer = "Spring";
+        } else if (6 <= m && m <= 8) {
+            answer = "Summer";
+        } else if (9 <= m && m <= 11) {
+            answer = "Fall";
+        } else {
+            answer = "Winter";
+        }
+
+        return answer;
+    }
+}
