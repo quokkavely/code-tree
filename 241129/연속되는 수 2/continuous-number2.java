a7ddca05986f9ff31e3c43d9d80a449e2c3d4f5f
Hyeji <165968530+quokkavely@users.noreply.github.com>
2024-11-29 11:44:10 +0900

    Title: 연속되는 수 2, Time: 178ms, Memory: 11MB, Status: Passed - CodeTree

--- 241129/연속되는 수 2/continuous-number2.java
+++ 241129/연속되는 수 2/continuous-number2.java
@@ -7,17 +7,22 @@ public class Main {
         
         int before = 0;
         int cnt = 1;
+        int max = 0;
         int[] arr = new int[n];
+
         for(int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
 
             if(arr[i] == before) {
                 cnt ++;
+            } else {
+                cnt = 1;
             }
 
+            max = Math.max(max, cnt);
             before = arr[i];
         }
 
-        System.out.print(cnt);
+        System.out.print(max);
     }
 }
