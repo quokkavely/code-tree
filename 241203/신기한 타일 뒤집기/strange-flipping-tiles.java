fc82765355b8fa9c7f63fc3be70c85be827fe9f9
Hyeji <165968530+quokkavely@users.noreply.github.com>
2024-12-03 08:57:02 +0900

    Title: 신기한 타일 뒤집기, Time: 203ms, Memory: 13MB, Status: Passed - CodeTree

--- 241203/신기한 타일 뒤집기/strange-flipping-tiles.java
+++ 241203/신기한 타일 뒤집기/strange-flipping-tiles.java
@@ -1,38 +1,34 @@
 import java.util.*;
 public class Main {
 
+    private static final Scanner sc= new Scanner(System.in);
+    private static int minV, maxV;
+    
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int loc = 0;
+        String direction = "";
         
-        Map<Integer, Character> map = new HashMap<>();
-        
-        for(int i = 0 ;  i < n ; i ++) {
-            int distance = sc.nextInt();
-            char direction = sc.next().charAt(0);
+        // n 총 개수
+        while( n-- > 0) {
             
-            if(direction == 'R') {
-                for(int j = loc; j < loc + distance ; j++) {
-                    map.put(j, 'B');
-                }
+            // 이동거리
+            int x = sc.nextInt() -1 ;
+            direction = sc.next();
+            
+            //다음 위치
+            int next = loc + (direction.equals("R")? x : -x);
 
-            } else {
-                distance = -1 * distance;
-                for(int j = loc; j > loc + distance ; j--) {
-                    map.put(j-1, 'W');
-                }
-            }
-            loc += distance;
+            minV = Math.min(minV, next);
+            maxV = Math.max(maxV, next);
+
+            loc = next; 
         }
-
-        int blackCnt = 0;
-        int whiteCnt = 0;
-        for(char value : map.values()) {
-            if(value == 'B') blackCnt++;
-            else if(value == 'W') whiteCnt++;
+   
+        if(direction.equals("R")) {
+            System.out.print((maxV - loc) + " " +(loc - minV + 1));
+        } else {
+            System.out.print((maxV - loc + 1) + " " +(loc - minV));
         }
-
-        System.out.print(whiteCnt+ " " + blackCnt); 
     }
 }
