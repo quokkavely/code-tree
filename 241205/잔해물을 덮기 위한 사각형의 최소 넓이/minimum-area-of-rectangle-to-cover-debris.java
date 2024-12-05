030ac054e2741dbb489afffc01ec752d7942835e
Hyeji <165968530+quokkavely@users.noreply.github.com>
2024-12-05 12:44:24 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241205/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 241205/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -16,25 +16,25 @@ public class Main {
         int by2 = sc.nextInt();
 
         //겹치는 부분
-        int overlapX1 = Math.max(ax1, bx1); //5
-        int overlapY1 = Math.max(ay1, by1); //1
-        int overlapX2 = Math.min(ax2, bx2); //7
-        int overlapY2 = Math.min(ay2, by2); //3
+        int overlapX1 = Math.max(ax1, bx1); 
+        int overlapY1 = Math.max(ay1, by1); 
+        int overlapX2 = Math.min(ax2, bx2); 
+        int overlapY2 = Math.min(ay2, by2); 
 
-        int answer = remainArea(ax1, ax2, overlapX1, overlapX2, ay1,ay2, overlapY1, overlapY2);
+        int answer = remainArea(ax1, ax2, overlapX1, overlapX2, ay1, ay2, overlapY1, overlapY2);
         System.out.println(answer);
     }
 
     private static int remainArea (int ax1, int ax2, int overlapX1, int overlapX2, int ay1, int ay2, int overlapY1, int overlapY2) {
-        if (ax2-ax1 < overlapX2-overlapX1 && ay2-ay1 < overlapY2-overlapY1) {
+        if (ax2 - ax1 <= overlapX2 - overlapX1 && ay2 - ay1 <= overlapY2 - overlapY1) {
             return 0; 
         } 
         //가로만 덮었을 때
-        else if(ax2 - ax1 < overlapX2 - overlapX1) {
+        else if(ax2 - ax1 <= overlapX2 - overlapX1) {
             return (ax2 - ax1) * (ay2 - overlapY2);
         } 
         //세로만 덮었을때
-        else if(ay2 - ay1 < overlapY2 - overlapY1) {
+        else if(ay2 - ay1 <= overlapY2 - overlapY1) {
             return (ax2 - overlapX2) * (ay2 - ay1);
         }
         else {
