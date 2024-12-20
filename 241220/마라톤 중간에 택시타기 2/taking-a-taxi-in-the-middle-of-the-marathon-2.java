77a845d24c651cf5dd0e0589ce9f7241e1c2c9e2
Hyeji <165968530+quokkavely@users.noreply.github.com>
2024-12-20 17:08:22 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 148ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241220/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        //한개만 건너뜀
+        int[][] arr = new int[n][2];
+        for(int i = 0; i < n; i ++) {
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+        
+        int x = arr[0][0];
+        int y = arr[0][1];
+        int distance = 0;
+        int min  = Integer.MAX_VALUE;
+        for(int i = 1; i < n ; i++) {
+            //i를 제외하고 거리 계산
+            for(int j = 1; j < n ; j ++) {
+                if( i != j ) {
+                    int nx = arr[j][0];
+                    int ny = arr[j][1];
+                    distance += Math.abs(nx-x) + Math.abs(ny - y);
+
+                    x = nx;
+                    y = ny;
+                }
+            }
+            min = Math.min(distance, min);
+        }
+
+        System.out.println(min);
+    }
+}
