c4150d688bcac63fc95b26eea9782146eff2e8fa
Hyeji <165968530+quokkavely@users.noreply.github.com>
2024-12-20 17:21:24 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 241220/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
+++ 241220/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
@@ -4,29 +4,33 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        //한개만 건너뜀
+        //첫번째 마지막 제외, 한개만 건너뜀
         int[][] arr = new int[n][2];
         for(int i = 0; i < n; i ++) {
             arr[i][0] = sc.nextInt();
             arr[i][1] = sc.nextInt();
         }
         
-        int x = arr[0][0];
-        int y = arr[0][1];
-        int distance = 0;
-        int min  = Integer.MAX_VALUE;
-        for(int i = 1; i < n ; i++) {
-            //i를 제외하고 거리 계산
-            for(int j = 1; j < n ; j ++) {
-                if( i != j || j == n-1) {
-                    int nx = arr[j][0];
-                    int ny = arr[j][1];
-                    distance += Math.abs(nx-x) + Math.abs(ny - y);
 
-                    x = nx;
-                    y = ny;
-                }
+        int min  = Integer.MAX_VALUE;
+        for(int i = 1; i < n -1 ; i++) { 
+            //i를 제외하고 거리 계산
+            int x = arr[0][0];
+            int y = arr[0][1];
+            int distance = 0;
+            
+            for(int j = 1; j < n ; j ++) { 
+                if( i == j ) continue;
+
+                int nx = arr[j][0];
+                int ny = arr[j][1];
+                distance += Math.abs(nx-x) + Math.abs(ny - y);
+
+                x = nx;
+                y = ny;
             }
+
+            distance += Math.abs(arr[n-1][0]-x) + Math.abs(arr[n-1][1] - y);
             min = Math.min(distance, min);
         }
 
